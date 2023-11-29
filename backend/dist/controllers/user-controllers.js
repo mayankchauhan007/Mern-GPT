9c8395b63af9c6a8dbf0da760e90aae685f470b0
mayankchauhan007 <mayankchauhan20837@gmail.com>
2023-11-29 16:08:04 +0530

    added api's for login and signup without authentication

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,3 +1,49 @@
-export const getAllUsers = () => {
+import User from "../models/User.js";
+import { compare, hash } from "bcrypt";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK !", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR !", cause: error.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        // user signup
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered !!");
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(201).json({ message: "OK !", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR !", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        // user login
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User is not registered.");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password !!");
+        }
+        return res.status(201).json({ message: "OK !", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR !", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
